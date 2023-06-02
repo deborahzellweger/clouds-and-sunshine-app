819f77480efcf0939731844ed5298c64ea540f8f
deborahzellweger <128902123+deborahzellweger@users.noreply.github.com>
2023-06-02 17:36:33 +0100

    added 'showTemp' function
    to display live data for temp, humidity and wind speed

--- clouds-and-sunshine-app/src/app.js
+++ clouds-and-sunshine-app/src/app.js
@@ -1,13 +1,20 @@
-function showTemp(response) {
+function showTempAndCity(response) {
+  console.log(response.data);
   let currentTemperature = document.querySelector("#current-temperature");
   currentTemperature.innerHTML = `${Math.round(
     response.data.temperature.current
   )}°`;
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `${response.data.wind.speed} KM/H`;
 }
 
-let city = "London";
+let city = "Bern";
 let apiKey = "fb48762bae7aac273c01t1cb80b143fo";
 let unit = "metric";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
 
-axios.get(apiUrl).then(showTemp);
+axios.get(apiUrl).then(showTempAndCity);
