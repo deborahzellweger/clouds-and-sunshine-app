a90c408d80b5996a47f74a509752acf30fc7e4de
deborahzellweger <128902123+deborahzellweger@users.noreply.github.com>
2023-06-09 14:47:14 +0100

    display forecast data using API

--- src/app.js
+++ src/app.js
@@ -13,20 +13,33 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatForecastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+  let day = days[date.getDay()];
+  return `${day}`;
+}
+
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
+  let forecastDetails = response.data.daily;
+
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecastDetails.forEach(function (forecastDay, index) {
+    if (index < 4) {
+      forecastHTML =
+        forecastHTML +
+        `
             <div class="col-3 g-0">
-                <span class="weekday">${day}</span>
+                <span class="weekday"> ${formatForecastDay(
+                  forecastDay.time
+                )}</span>
                 <div class="d-flex">
-                  <span class="max-temp"> 66 </span>
+                  <span class="max-temp"> ${Math.round(
+                    forecastDay.temperature.maximum
+                  )} </span>
                   <span class="min-temp">
-                    18 <br />
+                    ${Math.round(forecastDay.temperature.minimum)}  <br />
                     <img
                       class="weekday-1-icon"
                       src="src/weather-icons/weather-app-icons_few-clouds-day.png"
@@ -35,9 +48,18 @@ function displayForecast() {
                   </span>
                 </div>
               </div>`;
+    }
   });
   forecastHTML = forecastHTML + `<div/>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(city) {
+  let apiKey = "fb48762bae7aac273c01t1cb80b143fo";
+  let unit = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${unit}`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTempAndCity(response) {
@@ -48,8 +70,6 @@ function showTempAndCity(response) {
   let currentWindSpeed = document.querySelector("#wind");
   let currentDate = document.querySelector("#current-date");
   let mainWeatherIcon = document.querySelector("#main-weather-icon");
-
-  displayForecast();
 
   celsiusTemperature = response.data.temperature.current;
 
@@ -73,6 +93,8 @@ function showSearchInput(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
 
   axios.get(apiUrl).then(showTempAndCity);
+
+  getForecast(city);
 }
 
 function search(event) {
